9d0aae11e0f11f665f8d70552ad48eca0f117ba9
Amir Watad <Amir.Watad@gmail.com>
2020-03-23 01:13:52 +0200

    added growth rate graph

--- script.js
+++ script.js
@@ -32,49 +32,56 @@ function parseCasesCsvUrl(url, labels, cases) {
                 }
             }
 
-            drawCasesChart();
+            analyzeData();
+
         }
     })
 }
 
 function isRelevantDataKey(key) {
-    var startDate = moment("2/21/20", "M/D/YY", true);
-    var thisDate = moment(key, "M/D/YY", true);
+    var startDate = moment('2/21/20', 'M/D/YY', true);
+    var thisDate = moment(key, 'M/D/YY', true);
     var isDate = thisDate.isValid();
     var isAfterStartDate = thisDate.isSameOrAfter(startDate);
     return isDate && isAfterStartDate;
 }
 
 // cases Chart
-function drawCasesChart() {
-    if ((dateLabelsArr.length == 0) || (confirmedCasesArr.length == 0) || (deathsCsvUrl.length == 0) || (recoveredCsvUrl.length == 0)) {
+function analyzeData() {
+    if ((dateLabelsArr.length == 0) || (confirmedCasesArr.length == 0) || (deathCasesArr.length == 0) || (recoveredCasesArr.length == 0)) {
         return;
     }
 
+    drawCasesChart();
+
+    drawGrowthRateChart();
+}
+
+function drawCasesChart() {
     var ctx = document.getElementById('casesChart');
     var myLineChart = new Chart(ctx, {
         type: 'line',
         data: {
             datasets: [
             {
-                "label": "Confirmed Cases",
-                "fill":false,
-                "borderColor":"rgb(0, 150, 255)",
-                "lineTension":0.1,
+                'label': 'Confirmed Cases',
+                'fill':false,
+                'borderColor':'rgb(0, 150, 255)',
+                'lineTension':0.1,
                 data: confirmedCasesArr
             },
             {
-                "label": "Deaths",
-                "fill":false,
-                "borderColor":"rgb(187, 17, 0)",
-                "lineTension":0.1,
+                'label': 'Deaths',
+                'fill':false,
+                'borderColor':'rgb(187, 17, 0)',
+                'lineTension':0.1,
                 data: deathCasesArr
             },
             {
-                "label": "Recovered",
-                "fill":false,
-                "borderColor":"rgb(78, 143, 0)",
-                "lineTension":0.1,
+                'label': 'Recovered',
+                'fill':false,
+                'borderColor':'rgb(78, 143, 0)',
+                'lineTension':0.1,
                 data: recoveredCasesArr
             }
             ],
@@ -84,3 +91,51 @@ function drawCasesChart() {
     });
 }
 
+function drawGrowthRateChart() {
+    var growthRateArr = []
+    for (var i = 1; i < confirmedCasesArr.length; i++) {
+        if (confirmedCasesArr[i - 1] == 0) {
+            growthRateArr[i - 1] = 1;
+        } else {
+            growthRateArr[i - 1] = confirmedCasesArr[i] / confirmedCasesArr[i - 1];
+        }
+    }
+
+    meanGrowthRate = mean(growthRateArr);
+    meanGrowthRateArr = Array(growthRateArr.length).fill(meanGrowthRate);
+
+    console.log(meanGrowthRateArr);
+    console.log(growthRateArr);
+
+    var ctx = document.getElementById('growthRateChart');
+    var myLineChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            datasets: [
+            {
+                'label': 'Momentary Growth Rate',
+                'fill': false,
+                'borderColor': 'rgb(0, 150, 255)',
+                'lineTension': 0.1,
+                data: growthRateArr
+            },
+            {
+                'label': 'Mean Growth Rate',
+                'fill': false,
+                'borderColor':'rgb(187, 17, 0)',
+                'lineTension': 0.1,
+                'pointRadius': 0,
+                'borderDash': [5, 15],
+                data: meanGrowthRateArr
+            }
+            ],
+            labels: dateLabelsArr.splice(1)
+        },
+        options: {}
+    });
+}
+
+function mean(arr) {
+    var sum = arr.reduce((prev, curr) => prev += curr);
+    return sum / arr.length;
+}
